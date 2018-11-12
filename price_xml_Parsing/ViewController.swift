3fa4667bc0d4c559594a4cd7dcf93cb61850b0ff
Seo Sang won <33479927+zhaoSeo@users.noreply.github.com>
2018-11-12 12:36:22 +0900

    Initial Commit

---
+++ price_xml_Parsing/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  price_xml_Parsing
+//
+//  Created by Sang won Seo on 12/11/2018.
+//  Copyright © 2018 Sang won Seo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ price_xml_ParsingTests/price_xml_ParsingTests.swift
@@ -0,0 +1,34 @@
+//
+//  price_xml_ParsingTests.swift
+//  price_xml_ParsingTests
+//
+//  Created by Sang won Seo on 12/11/2018.
+//  Copyright © 2018 Sang won Seo. All rights reserved.
+//
+
+import XCTest
+@testable import price_xml_Parsing
+
+class price_xml_ParsingTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
